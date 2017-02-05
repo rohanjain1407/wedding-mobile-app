b74bacd645c548b59517693ce3d41d91d2a6d5d0
Neel Jain <neeljain198@gmail.com>
2017-02-05 17:57:19 -0500

    store device token to db

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,17 +7,38 @@ import {
   PushToken
 } from '@ionic/cloud-angular';
 
+import { WeddingApi } from '../../providers/wedding-api'
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [WeddingApi]
 })
-export class HomePage {
 
-  	constructor(public navCtrl: NavController, public push: Push) {
+export class HomePage {
+	weddingId: any; 
+  	constructor(public navCtrl: NavController, public push: Push, private weddingApi: WeddingApi) {
 	    this.push.register().then((t: PushToken) => {
 	  		return this.push.saveToken(t);
 		}).then((t: PushToken) => {
 	  		console.log('Token saved:', t.token);
 		});
+	}
+
+	register() {
+		console.log('register');
+		if (this.push.token != undefined) {
+			console.log("weddingID:" + this.weddingId);
+			this.weddingApi.registerToken(this.weddingId, this.push.token.token).subscribe(
+				data => {
+					console.log(data);
+				},
+				err => {
+					console.log('ERROR:'+err.toString());
+			});
+		}
+		else {
+			console.log('Error cannot register, Token undefined');
+		}
 	}
 }
