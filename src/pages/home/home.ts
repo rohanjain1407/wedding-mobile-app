ebfcac9a742a6f838ab3deaf29fc4e98afd26a83
Rohan Jain <rohanjain.1407@gmail.com>
2017-02-12 18:04:48 -0500

    WED-14 : Error handling in cases when wedding id entered is invalid

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
-
-import {
-  Push,
-  PushToken
-} from '@ionic/cloud-angular';
-
+import { LoadingController, ToastController, NavController } from 'ionic-angular';
+import { Push, PushToken} from '@ionic/cloud-angular';
 import { WeddingApi } from '../../providers/wedding-api'
 
 @Component({
@@ -15,9 +9,10 @@ import { WeddingApi } from '../../providers/wedding-api'
   providers: [WeddingApi]
 })
 
-export class HomePage {
+export class HomePage {	
 	weddingId: any; 
-  	constructor(public navCtrl: NavController, public push: Push, private weddingApi: WeddingApi) {
+  	constructor(public navCtrl: NavController, public push: Push, private weddingApi: WeddingApi,
+  		public toastCtrl: ToastController, public loadingCtrl: LoadingController) {
 	    this.push.register().then((t: PushToken) => {
 	  		return this.push.saveToken(t);
 		}).then((t: PushToken) => {
@@ -26,19 +21,57 @@ export class HomePage {
 	}
 
 	register() {
-		console.log('register');
+		//Set loader to end in 10 seconds. Stop it manually by calling dismiss
+		let loading = this.loadingCtrl.create({
+    	content: 'Please wait...',
+    	dismissOnPageChange: true,
+    	duration: 10000
+    	});
+  		loading.present();
+
 		if (this.push.token != undefined) {
-			console.log("weddingID:" + this.weddingId);
-			this.weddingApi.registerToken(this.weddingId, this.push.token.token).subscribe(
+			this.weddingApi.registerToken(this.weddingId, "tryyy").subscribe(
 				data => {
+					loading.dismiss();
+					var error = data["error"];
+					if(error) {
+						this.handleError(error);
+					}
 					console.log(data);
 				},
 				err => {
-					console.log('ERROR:'+err.toString());
+					loading.dismiss();
+					this.handleError("CONNECTION_ERROR");
+					console.log('ERROR:'+ err.toString());
 			});
 		}
 		else {
 			console.log('Error cannot register, Token undefined');
 		}
+	}
+
+	handleError(error: string) {
+		var errorMessage;
+		switch (error) {
+			case "INVALID_ENTRY":
+				errorMessage = "We do not recognize this Wedding ID. Please try again.";
+				break;
+				
+			case "CONNECTION_ERROR":
+				errorMessage = "The server is unreachable. Please try again."	
+				break;	
+			
+			case "UNKNOWN":
+			default:
+				errorMessage = "Error occurred. Please try again.";
+				break;
+		}
+
+		let toast = this.toastCtrl.create({
+    	message: errorMessage,
+     	duration: 3000,
+     	position:"top"
+    	});
+    	toast.present();	
 	}
 }
