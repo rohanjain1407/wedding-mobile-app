17596b1e00483036c0f75546ca3f2cf48c187771
Rohan Jain <rohanjain.1407@gmail.com>
2017-02-17 21:33:48 -0500

    WED-14 : code review changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,58 +20,55 @@ export class HomePage {
 		});
 	}
 
-	register() {
-		//Set loader to end in 10 seconds. Stop it manually by calling dismiss
-		let loading = this.loadingCtrl.create({
-    	content: 'Please wait...',
-    	dismissOnPageChange: true,
-    	duration: 10000
-    	});
-  		loading.present();
+  register() {
+    //Set loader to end in 10 seconds. Stop it manually by calling dismiss
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true,
+      duration: 10000
+    });
+    loading.present();
 
-		if (this.push.token != undefined) {
-			this.weddingApi.registerToken(this.weddingId, this.push.token.token).subscribe(
-				data => {
-					loading.dismiss();
-					var error = data["error"];
-					if(error) {
-						this.handleError(error);
-					}
-					console.log(data);
-				},
-				err => {
-					loading.dismiss();
-					this.handleError("CONNECTION_ERROR");
-					console.log('ERROR:'+ err.toString());
-			});
-		}
-		else {
-			console.log('Error cannot register, Token undefined');
-		}
-	}
+    if (this.push.token != undefined) {
+      this.weddingApi.registerToken(this.weddingId, this.push.token.token).subscribe(
+          data => {
+          loading.dismiss();
+          console.log(data);
+        },
+          err => {
+          loading.dismiss();
+          var err = err.json();
+          var error = err["error"] != null ? err["error"] : "CONNECTION_ERROR";
+          this.handleError(error);
+        });
+    }
+    else {
+      console.log('Error cannot register, Token undefined');
+    }
+  }
 
-	handleError(error: string) {
-		var errorMessage;
-		switch (error) {
-			case "INVALID_ENTRY":
-				errorMessage = "We do not recognize this Wedding ID. Please try again.";
-				break;
+  handleError(error: string) {
+    var errorMessage;
+    switch (error) {
+      case "RECORD_NOT_FOUND":
+        errorMessage = "We do not recognize this Wedding ID. Please try again.";
+        break;
 
-			case "CONNECTION_ERROR":
-				errorMessage = "The server is unreachable. Please try again."
-				break;
+      case "CONNECTION_ERROR":
+        errorMessage = "The server is unreachable. Please try again."
+        break;
 
-			case "UNKNOWN":
-			default:
-				errorMessage = "Error occurred. Please try again.";
-				break;
-		}
+      case "UNKNOWN":
+      default:
+        errorMessage = "Error occurred. Please try again.";
+        break;
+    }
 
-		let toast = this.toastCtrl.create({
-    	message: errorMessage,
-     	duration: 3000,
-     	position:"top"
-    	});
-    	toast.present();
-	}
+    let toast = this.toastCtrl.create({
+      message: errorMessage,
+      duration: 3000,
+      position:"top"
+    });
+    toast.present();
+  }
 }
